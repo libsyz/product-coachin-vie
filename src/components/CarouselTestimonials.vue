f73917a82d72c7f65c6ef91fd95d1944752130cd
Miguel Jimenez <denkatalabeno@gmail.com>
2019-12-07 22:46:38 +0800

    added basic layouting to testimonials component

--- src/components/CarouselTestimonials.vue
+++ src/components/CarouselTestimonials.vue
@@ -1,6 +1,5 @@
 
 <template>
-
   <div class="card-carousel-wrapper">
     <div class="card-carousel--nav__left"
          @click="moveCarousel(-1)"
@@ -11,16 +10,20 @@
         <div class="card-carousel-cards"
              :style="{ transform: 'translateX' + '(' + currentOffset + 'px' + ')'}">
           <div class="card-carousel--card"
-               v-for="item in items"
-               :key="item.id">
+               v-for="testimonial in items"
+               :key="testimonial.id">
 
-               <img src="https://placehold.it/200x200"/>
-            <div class="card-carousel--card--footer">
-              <p>{{ item.name }}</p>
-              <p class="tag"
-              v-for="(tag,index) in item.tag"
-              :key="tag"
-              :class="index &gt; 0 ? 'secondary' : ''">{{ tag }}</p>
+            <img src="https://placehold.it/200x200"/>
+            <div class="card-carousel--card--information">
+              <p class="information-testimonial">
+              </p>
+              <p class="information-referrer">
+                {{ testimonial.blurb }}
+              </p>
+              <p>
+                {{ testimonial.referrer}}
+              </p>
+
             </div>
           </div>
         </div>
@@ -44,32 +47,32 @@
         windowSize: 3,
         paginationFactor: 220,
         items: [
-          {id:1, name: 'Kin Khao', tag: ["Thai"]},
-          {id:2, name: 'Jū-Ni', tag: ["Sushi", "Japanese", "$$$$"]},
-          {id:3, name: 'Delfina', tag: ["Pizza", "Casual"]},
-          {id:4, name: 'San Tung', tag: ["Chinese", "$$"]},
-          {id:5, name: 'Anchor Oyster Bar', tag: ["Seafood", "Cioppino"]},
-          {id:6, name: 'Locanda', tag: ["Italian"]},
-          {id:7, name: 'Garden Creamery', tag: ["Ice cream"]},
+          {id:1, blurb: 'Kin Khao', referrer: "Aaron Johnson"},
+          {id:2, blurb: 'Jū-Ni', referrer: "Aaron Johnson"},
+          {id:3, blurb: 'Delfina', referrer: "Aaron Johnson"},
+          {id:4, blurb: 'San Tung', referrer: "Aaron Johnson"},
+          {id:5, blurb: 'Anchor Oyster Bar', referrer: "Aaron Johnson"},
+          {id:6, blurb: 'Locanda', referrer: "Aaron Johnson"},
+          {id:7, blurb: 'Garden Creamery', referrer: "Aaron Johnson"},
         ]
       }
     },
     computed: {
-    atEndOfList() {
-      return this.currentOffset <= (this.paginationFactor * -1) * (this.items.length - this.windowSize);
+      atEndOfList() {
+        return this.currentOffset <= (this.paginationFactor * -1) * (this.items.length - this.windowSize);
+      },
+      atHeadOfList() {
+        return this.currentOffset === 0;
+      },
     },
-    atHeadOfList() {
-      return this.currentOffset === 0;
-    },
-  },
-  methods: {
-    moveCarousel(direction) {
+    methods: {
+      moveCarousel(direction) {
       // Find a more elegant way to express the :style. consider using props to make it truly generic
-      if (direction === 1 && !this.atEndOfList) {
-        this.currentOffset -= this.paginationFactor;
-      } else if (direction === -1 && !this.atHeadOfList) {
-        this.currentOffset += this.paginationFactor;
-      }
+        if (direction === 1 && !this.atEndOfList) {
+          this.currentOffset -= this.paginationFactor;
+        } else if (direction === -1 && !this.atHeadOfList) {
+          this.currentOffset += this.paginationFactor;
+        }
     },
   }
 }
@@ -78,25 +81,21 @@
 
 <style scoped>
 
-body {
-  background: #f8f8f8;
-  color: #2c3e50;
-  font-family: 'Source Sans Pro', sans-serif;
-}
-
 .card-carousel-wrapper {
   display: flex;
   align-items: center;
   justify-content: center;
   margin: 20px 0 40px;
+  padding-top: 40px;
   color: #666a73;
 }
 
 .card-carousel {
   display: flex;
   justify-content: center;
-  width: 640px;
+  width: 800px;
 }
+
 .card-carousel--overflow-container {
   overflow: hidden;
 }
@@ -134,85 +133,37 @@ body {
   transition: transform 150ms ease-out;
   transform: translatex(0px);
 }
-.card-carousel-cards .card-carousel--card {
+
+.card-carousel--card {
+  display: flex;
+  min-width: 600px;
   margin: 0 10px;
   cursor: pointer;
-  box-shadow: 0 4px 15px 0 rgba(40, 44, 53, 0.06), 0 2px 2px 0 rgba(40, 44, 53, 0.08);
+  box-shadow: 0 4px 15px 0 rgba(40, 44, 53, 0.06),
+              0 2px 2px 0 rgba(40, 44, 53, 0.08);
   background-color: #fff;
   border-radius: 4px;
   z-index: 3;
   margin-bottom: 2px;
 }
-.card-carousel-cards .card-carousel--card:first-child {
+.card-carousel--card:first-child {
   margin-left: 0;
 }
-.card-carousel-cards .card-carousel--card:last-child {
+.card-carousel--card:last-child {
   margin-right: 0;
 }
-.card-carousel-cards .card-carousel--card img {
+ .card-carousel--card img {
   vertical-align: bottom;
   border-top-left-radius: 4px;
   border-top-right-radius: 4px;
   transition: opacity 150ms linear;
   user-select: none;
 }
-.card-carousel-cards .card-carousel--card img:hover {
+.card-carousel--card img:hover {
   opacity: 0.5;
 }
-.card-carousel-cards .card-carousel--card--footer {
-  border-top: 0;
-  padding: 7px 15px;
-}
-.card-carousel-cards .card-carousel--card--footer p {
-  padding: 3px 0;
-  margin: 0;
-  margin-bottom: 2px;
-  font-size: 19px;
-  font-weight: 500;
-  color: #2c3e50;
-  user-select: none;
-}
-.card-carousel-cards .card-carousel--card--footer p.tag {
-  font-size: 11px;
-  font-weight: 300;
-  padding: 4px;
-  background: rgba(40, 44, 53, 0.06);
-  display: inline-block;
-  position: relative;
-  margin-left: 4px;
-  color: #666a73;
-}
-.card-carousel-cards .card-carousel--card--footer p.tag:before {
-  content: "";
-  float: left;
-  position: absolute;
-  top: 0;
-  left: -12px;
-  width: 0;
-  height: 0;
-  border-color: transparent rgba(40, 44, 53, 0.06) transparent transparent;
-  border-style: solid;
-  border-width: 8px 12px 12px 0;
-}
-.card-carousel-cards .card-carousel--card--footer p.tag.secondary {
-  margin-left: 0;
-  border-left: 1.45px dashed white;
-}
-.card-carousel-cards .card-carousel--card--footer p.tag.secondary:before {
-  display: none !important;
-}
-.card-carousel-cards .card-carousel--card--footer p.tag:after {
-  content: "";
-  position: absolute;
-  top: 8px;
-  left: -3px;
-  float: left;
-  width: 4px;
-  height: 4px;
-  border-radius: 2px;
-  background: white;
-  box-shadow: -0px -0px 0px #004977;
-}
+
+
 
 h1 {
   font-size: 3.6em;
@@ -221,6 +172,5 @@ h1 {
   margin-bottom: 0;
   color: #42b883;
 }
-
 
 </style>
