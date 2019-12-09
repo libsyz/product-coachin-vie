28d980d1861fcd38f617692eef409430be13dd22
Miguel Jimenez <denkatalabeno@gmail.com>
2019-12-09 19:23:01 +0800

    implement routerview

---
+++ src/pages/ForCompanies.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <section class="for-companies">
+    <h1>for-companies Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'for-companies',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
