263418a871cfa4e07ca083ffb123788a2a2a6e5d
Miguel Jimenez <denkatalabeno@gmail.com>
2019-12-10 13:19:53 +0800

    added sandbox

---
+++ src/components/Framework.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="framework">
+    <h1>framework Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'framework',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped>
+  .framework {
+
+  }
+</style>
