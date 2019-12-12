8e8f502a7f5c332f7c2ba18694e5bef9e03f8d99
jumpingrock <kennethyeongky@gmail.com>
2019-12-12 18:26:23 +0800

    added workshop and pricing

---
+++ src/components/CardforIndividual.vue
@@ -0,0 +1,61 @@
+
+<template>
+  <div class='card'>
+    <img :src="imgUrl" alt="Why not rendering">
+    <h2> {{ header }}
+      <span class="text-underline"> </span>
+    </h2>
+    
+    <p> {{ content }}</p>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'card-product',
+    props: {
+      header: String,
+      content: String,
+      imgUrl: String
+    }
+  }
+</script>
+
+
+<style scoped>
+
+  .card {
+    width: 350px;
+    height: 400px;
+    display: inline-block;
+    flex-direction: column;
+    justify-content: center;
+    padding: 50px;
+    margin: 25px;
+    border-radius: 4px;
+    box-shadow: 0 15px 35px rgba(126,87,88,.1),
+              0 5px 15px rgba(0,0,0,.07);
+  }
+  .text-underline {
+  position: absolute;
+  width: 33%;
+  max-height: 1px;
+  border: solid 1px #000;
+  bottom: 25px;
+  left: 33%;
+  }
+
+  h2 {
+    text-transform: uppercase;
+    padding-bottom: 30px;
+    position: relative;
+  }
+
+  img {
+    height: 125px;
+    padding-bottom: 30px
+  }
+  
+
+</style>
--- src/pages/for-individuals/ForIndividuals.vue
+++ src/pages/for-individuals/ForIndividuals.vue
@@ -11,7 +11,33 @@
                    :headerText="card.headerText"
                    :bodyText="card.bodyText"/>
     </div>
+    <div class="workshops ">
+      <h2>Some of Our Workshops</h2>
 
+      <CardforIndividual v-for="workshop in workshops" :key="workshop.id"
+                        :header="workshop.headerText"
+                        :content="workshop.bodyText"
+                        :imgUrl="workshop.imgUrl"/>
+    </div>
+    <div class="parent">
+      <div class="div1 info-column">1-Day Workshops </div>
+      <div class="div2 price-column">$200 SGD / PERSON </div>
+      <div class="div3 info-column">Multi-Day Workshops </div>
+      <div class="div4 price-column">$150 SGD / PERSON </div>
+      <div class="div5 info-column">Individual Coaching </div>
+      <div class="div6 price-column" id="requestRect">Depending on request </div>
+      <div class="div7 "> 
+        <h4>WHAT'S INCULDED</h4>
+        <ul>
+          <li>Certificate of Completion</li>
+          <li>Lifetime access to our resource library</li>
+          <li>Life time access to our slack community</li>
+          <li>Lunch and Coffee Breaks! *important*</li>
+          <li>An Awesome Mavericks T-Shirt</li>
+        </ul>
+      </div>
+    </div>
+    
 
     <StayInTouch header="Ready to uplift your product teams?"
                  body="We offer free 1-day taster workshops. Contact us to know more"
@@ -32,15 +58,17 @@
   import CardProduct from './../../components/CardProduct.vue'
   import StayInTouch from './../../components/StayInTouch.vue'
   import CarouselCaseStudies from './../../components/CarouselCaseStudies.vue'
+  import CardforIndividual from './../../components/CardforIndividual.vue'
 
   export default  {
-    name: 'for-companies',
+    name: 'for-individual',
     components: {
       HeroPage,
       BlockQuote,
       CardProduct,
       StayInTouch,
-      CarouselCaseStudies
+      CarouselCaseStudies,
+      CardforIndividual
     },
     data () {
       return {
@@ -59,8 +87,30 @@
             imgUrl: require('./../../assets/for-companies/plant.png'),
             headerText: "1 ON 1 COACHING",
             bodyText: "Hire a coach on demand that unleash your best thinking and helps you frame the most challenging product conversations"
+          }
+        ],
+        workshops: [
+          {
+            imgUrl: require('./../../assets/landing/launch-2.png'),
+            headerText: "Roadmap 101",
+            bodyText: "Create and communicate agile product roadmaps that balance flexibility and execution."
           },
-        ]
+          {
+            imgUrl: require('./../../assets/for-companies/plant.png'),
+            headerText: "Roadmap 102",
+            bodyText: "Create and communicate agile product roadmaps that balance flexibility and execution."
+          },
+          {
+            imgUrl: require('./../../assets/for-companies/plant.png'),
+            headerText: "Roadmap 103",
+            bodyText: "Create and communicate agile product roadmaps that balance flexibility and execution."
+          },
+          {
+            imgUrl: require('./../../assets/for-companies/plant.png'),
+            headerText: "Roadmap 104",
+            bodyText: "Create and communicate agile product roadmaps that balance flexibility and execution."
+          }
+        ],
       }
     }
 }
@@ -77,6 +127,94 @@
   background-color: #F0EFFF;
 
 }
+.workshopItem {
+  display: inline-block;
+  margin: 25px;
+  padding: 25px;
+}
+
+.parent {
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+grid-template-rows: repeat(3, 1fr);
+grid-column-gap: 10px;
+grid-row-gap: 10px;
+margin-top: 40px;
+padding-top: 80px;
+padding-left: 223px;
+background: #F0EFFF;
+}
+
+.div1 { grid-area: 1 / 1 / 2 / 2; }
+.div2 { grid-area: 1 / 2 / 2 / 3; }
+.div3 { grid-area: 2 / 1 / 3 / 2; }
+.div4 { grid-area: 2 / 2 / 3 / 3; }
+.div5 { grid-area: 3 / 1 / 4 / 2; }
+.div6 { grid-area: 3 / 2 / 4 / 4; }
+.div7 { grid-area: 1 / 3 / 3 / 4; 
+  /* position: absolute; */
+  width: 327px;
+  height: 230px;
+  /* left: 985px;
+  top: 2983px; */
+  text-align: left;
+  background: #FFFFFF;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 22px;
+
+  color: #000000;
+}
+
+
+.price-column {
+  
+  width: 359px;
+  height: 106px;
+
+  background: #FFFFFF;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  padding-left: 20px;
+
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 27px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #000000;
+
+}
+.info-column {
+  width: 381px;
+  height: 106px;
+
+  background: #6C63FF;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  padding-left: 20px;
+
+  font-style: normal;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 27px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #FFFFFF;
+}
+
+#requestRect {
+  width: 737px;
+}
+
 
 
 </style>
