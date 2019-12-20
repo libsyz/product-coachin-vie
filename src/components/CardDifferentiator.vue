22366effeda480abc98574db164ac0abf310e70b
Miguel Jimenez <denkatalabeno@gmail.com>
2019-12-20 17:15:43 +0800

    added mapbox component, working well

--- src/components/CardDifferentiator.vue
+++ src/components/CardDifferentiator.vue
@@ -1,17 +1,12 @@
+
 <template lang="html">
-
   <section class="card-differentiator">
-
     <img :src="url" />
-  <div class="card-differentiator-infos">
-    <h2>{{ header }}</h2>
-    <p> {{ description }}</p>
-
-</div>
-
-
+    <div class="card-differentiator-infos">
+      <h2>{{ header }}</h2>
+      <p> {{ body }}</p>
+    </div>
   </section>
-
 </template>
 
 <script lang="js">
@@ -21,7 +16,10 @@
     props: {
       header: String,
       body: String,
-      url: String
+      url: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1547447134-cd3f5c716030?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
+      }
     },
     mounted () {
 
@@ -42,38 +40,46 @@
 
 </script>
 
-<style scoped lang="scss">
+<style scoped>
   .card-differentiator {
-  overflow: hidden;
-  height: 120px;
-  background: white;
-  box-shadow: 0 0 15px rgba(0,0,0,0.2);
-  display: flex;
-  align-items: center;
+    overflow: hidden;
+    height: 300px;
+    min-width: 400px;
+    background: white;
+    box-shadow: 0 15px 35px rgba(126, 87, 88, 0.1)
+              , 0 5px 15px rgba(0, 0, 0, 0.07);
+    display: flex;
+    align-items: center;
 }
 
 .card-differentiator img {
   height: 100%;
-  width: 120px;
+  width: 50%;
   object-fit: cover;
 }
 
 .card-differentiator h2 {
+  text-transform: uppercase;
   font-size: 16px;
   font-weight: bold;
   margin: 0;
 }
 
 .card-differentiator p {
-  font-size: 12px;
+  font-size: 1rem;
   line-height: 1.4;
-  opacity: .7;
   margin-bottom: 0;
   margin-top: 8px;
 }
 
 .card-differentiator .card-differentiator-infos {
   padding: 16px;
+}
+
+@media(min-width: 572px) {
+  .card-differentiator {
+    margin-bottom: 20px;
+  }
 }
 
 </style>
---
+++ src/components/mapbox.vue
@@ -0,0 +1,60 @@
+<template lang="html">
+
+  <section class="mapbox">
+    <div id="map"></div>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+  mapboxgl.accessToken = 'pk.eyJ1IjoibGlic3l6bWF2ZXJpY2tzIiwiYSI6ImNrNGR2Nm9qeTA3MHYzZW5vMnR2ajZ4dG4ifQ.Xc57Kii_RyyCpkwe4yUYVg'
+
+  export default  {
+    name: 'mapbox',
+    props: [],
+    mounted () {
+      const map = new mapboxgl.Map({
+          container: 'map',
+          center: [103.818213,1.26496],
+          zoom: 16,
+          style: 'mapbox://styles/mapbox/streets-v11'
+        })
+
+      new mapboxgl.Marker({
+        color: "#BADA55"
+      })
+      .setLngLat([103.818213, 1.26496])
+      .addTo(map)
+
+
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+ }
+
+</script>
+
+<style scoped>
+
+
+  .mapbox {
+
+  }
+
+  #map {
+    min-height: 400px;
+    min-width: 400px;
+  }
+</style>
